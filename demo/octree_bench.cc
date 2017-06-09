a318ec559b6093427fab88ac7e20c107bff6ab35
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2017-06-09 15:33:56 +0200

    Comment PCL lib to avoid installation

--- demo/octree_bench.cc
+++ demo/octree_bench.cc
@@ -1,8 +1,9 @@
+#include "omp.h"
 #include "cell.h"
 #include "inline_vector.h"
 #include "neighbor_nanoflann_op.h"
 #include "neighbor_op.h"
-#include "neighbor_pcl_op.h"
+// #include "neighbor_pcl_op.h"
 #include "neighbor_unibn_op.h"
 
 #include <chrono>
@@ -15,7 +16,7 @@ using std::ofstream;
 using bdm::Cell;
 using bdm::NeighborOp;
 using bdm::NeighborNanoflannOp;
-using bdm::NeighborPclOp;
+// using bdm::NeighborPclOp;
 using bdm::NeighborUnibnOp;
 using bdm::Scalar;
 
@@ -27,7 +28,7 @@ void RunTest(T* cells, const Op& op, char filename[100], int threads) {
   std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
   if (print_terminal == 1) {
-    std::cout << "op.Compute           = " 
+    std::cout << "op.Compute           = "
               << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()
               << "ms\n"
               << "=============================================\n\n";
@@ -53,16 +54,16 @@ int main(int args, char** argv) {
 
     auto cells = Cell<>::NewEmptySoa();
     cells.reserve(cells_per_dim * cells_per_dim * cells_per_dim);
-    
+
     const double space = 20;
     for (size_t i = 0; i < cells_per_dim; i++) {
       for (size_t j = 0; j < cells_per_dim; j++) {
         for (size_t k = 0; k < cells_per_dim; k++) {
           Cell<Scalar> cell({i * space, j * space, k * space});
-          cell.SetDiameter(30);
-          cell.SetAdherence(0.4);
-          cell.SetMass(1.0);
-          cell.UpdateVolume();
+          // cell.SetDiameter(30);
+          // cell.SetAdherence(0.4);
+          // cell.SetMass(1.0);
+          // cell.UpdateVolume();
           cells.push_back(cell);
         }
       }
@@ -71,7 +72,7 @@ int main(int args, char** argv) {
     if (print_terminal == 1) {
       std::cout << "Created grid of [" << (cells_per_dim*cells_per_dim*cells_per_dim) << "] cells\n" << std::endl;
     }
-    
+
     const char* dirname = argv[3];
     mkdir(dirname, 0700);
 
@@ -83,17 +84,17 @@ int main(int args, char** argv) {
     strcpy(neighbor_nf_file, dirname);
     strcat(neighbor_nf_file, "/NanoflannOp.txt");
 
-    char neighbor_pcl_file[100];
-    strcpy(neighbor_pcl_file, dirname);
-    strcat(neighbor_pcl_file, "/PCL.txt");
+    // char neighbor_pcl_file[100];
+    // strcpy(neighbor_pcl_file, dirname);
+    // strcat(neighbor_pcl_file, "/PCL.txt");
 
     char neighbor_unibn_file[100];
     strcpy(neighbor_unibn_file, dirname);
     strcat(neighbor_unibn_file, "/UniBn.txt");
 
-    RunTest(&cells, NeighborOp(700), neighbor_op_file, threads);
-    RunTest(&cells, NeighborNanoflannOp(700), neighbor_nf_file, threads);
-    RunTest(&cells, NeighborPclOp(700), neighbor_pcl_file, threads);
+    // RunTest(&cells, NeighborOp(700), neighbor_op_file, threads);
+    // RunTest(&cells, NeighborNanoflannOp(700), neighbor_nf_file, threads);
+    // RunTest(&cells, NeighborPclOp(700), neighbor_pcl_file, threads);
     RunTest(&cells, NeighborUnibnOp(700), neighbor_unibn_file, threads);
   } else {
     std::cout << "Error args: ./octree_bench <cells_per_dim> <num_treads> <dirname>" << std::endl;
