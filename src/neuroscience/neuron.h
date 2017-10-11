367c74b5f988e6ab01d4d0eb945ce8a6c90e495e
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2017-10-11 21:19:10 +0200

    Solve issue with circular dependency and incomplete types

--- src/neuroscience/neuron.h
+++ src/neuroscience/neuron.h
@@ -7,11 +7,12 @@
 
 namespace bdm {
 
+namespace neuroscience {
+
 BDM_SIM_CLASS(Neuron, Cell) {
-  BDM_CLASS_HEADER(NeuronExt, 1, daughters_, foo_, aa_);
+  BDM_CLASS_HEADER(NeuronExt, 1, daughters_, foo_);
 
  public:
-  using SimBackend = typename TCompileTimeParam::SimulationBackend;
   using TNeurite = typename TCompileTimeParam::TNeurite;
 
   NeuronExt() {
@@ -23,13 +24,13 @@ BDM_SIM_CLASS(Neuron, Cell) {
  private:
   // vec<SoPointer<typename ToBackend<TNeurite, SimBackend>::type, SimBackend>>
   // daughters_;
-  vec<SoPointer<TNeurite, SimBackend>> daughters_;
+  vec<SoPointer<ToBackend<TNeurite, SimBackend>, SimBackend>> daughters_;
   // TNeurite* bar_;
 
-  vec<SoPointer<typename TNeurite::template Self<Scalar>, SimBackend>> aa_;
+  // vec<SoPointer<typename TNeurite::template Self<SimBackend>, SimBackend>> aa_;
 
   // vec<SoPointer<TNeuron<SimBackend>, SimBackend>> foo_;
-  vec<SoPointer<MostDerived, SimBackend>> foo_;
+  vec<MostDerivedSoPtr> foo_;
   // vec<MostDerived*> foo_;
   // using TNeuron instead of Self<Backend> in case a customized neuron is used
   //  vec<SoPointer<TNeuron, SimBackend>> bar_;
@@ -40,13 +41,15 @@ BDM_SIM_CLASS(SpecializedNeuron, Neuron) {
   BDM_CLASS_HEADER(SpecializedNeuronExt, 1, me_);
 
  public:
-  using SimBackend = typename TCompileTimeParam::SimulationBackend;
-  SpecializedNeuronExt() { auto&& me = me_[kIdx].Get(); }
+  SpecializedNeuronExt() {
+    auto&& me = me_[kIdx].Get();
+  }
 
  private:
-  vec<SoPointer<MostDerived, SimBackend>> me_;
+  vec<SoPointer<MostDerivedSB, SimBackend>> me_;
 };
 
+}  // namespace neuroscience
 }  // namespace bdm
 
 #endif  // NEUROSCIENCE_NEURON_H_
